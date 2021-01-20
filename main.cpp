3f8f16eadaca79b91eb7b8785c6a8a8a1c5f83bc
Medpd <methe.medpd@gmail.com>
2021-01-20 01:51:58 +0100

    new challenge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+//!  code challenge URL https://www.hackerrank.com/challenges/virtual-functions/problem
 using namespace std;
+
+class Person{
+protected:
+    string name;
+    int age;
+
+public: 
+    Person(string name, int age){
+        this->name = name; this->age = age;
+    }
+
+};
+
+class Professor: Person{
+private: 
+    string publications;
+    static int id_inc; // unique ID, this prop is a class prop and not an object prop
+    const int cur_id;
+public:
+    Professor(string name, int age, string pub): Person(name, age), cur_id(++id_inc){
+        this->publications = pub; 
+    }
+    Professor getdata(){
+        string name, pub; int age;
+        cin >> name >> pub >> age;
+        return Professor(name, age, pub);
+    }
+    void putData(){
+
+    }
+};
+
+class Student: Person{
+
+};
+
 
 
 int main(int argc, char const *argv[])
